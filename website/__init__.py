b33060874374243736f299b7835be7bde1215a31
Kumar Mayank <92620671+ruins00@users.noreply.github.com>
2023-05-04 17:41:57 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,7 +11,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL  #f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']  #f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
